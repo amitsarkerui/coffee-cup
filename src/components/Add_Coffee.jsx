aa1a99245220522eb9841892c4de9017aa436766
Amit Sarker <amitsarkerstock@gmail.com>
2023-05-13 13:24:36 +0600

    data post to mongoDB

--- src/components/Add_Coffee.jsx
+++ src/components/Add_Coffee.jsx
@@ -1,9 +1,166 @@
 import React from "react";
+// import { data } from "autoprefixer";
+import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const Add_Coffee = () => {
+  const handleAddCoffee = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const chef = form.chef.value;
+    const supplier = form.supplier.value;
+    const taste = form.taste.value;
+    const category = form.category.value;
+    const details = form.details.value;
+    const photo = form.photo.value;
+
+    const newCoffee = { name, chef, supplier, taste, category, details, photo };
+    console.log(newCoffee);
+
+    fetch("http://localhost:5000/coffee", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newCoffee),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.insertedId) {
+          Swal.fire({
+            title: "Coffee added successfully !",
+            text: "Do you want to continue",
+            icon: "success",
+            confirmButtonText: "Yeah!",
+          });
+        }
+      });
+  };
   return (
-    <div>
-      <h1>Add Coffee</h1>
+    <div className="container mx-auto py-20">
+      <Link to={"/"}>
+        <span className="flex items-center gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-5 h-5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+            />
+          </svg>
+          <h4>Back to home</h4>
+        </span>
+      </Link>
+      <div className="bg-gray-200 mt-10 px-10 p-12 rounded-xl">
+        <h2 className="text-2xl text-center">Add New Coffee</h2>
+        <p className="text-center max-w-2xl mx-auto my-5 text-gray-600">
+          It is a long established fact that a reader will be distraceted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsum is that it has a more-or-less normal distribution of
+          letters, as opposed to using Content here.
+        </p>
+        <div>
+          <form
+            onSubmit={handleAddCoffee}
+            className="grid grid-cols-1 gap-6 md:grid-cols-2"
+          >
+            <div className="">
+              <label className="mb-3 ml-1 block text-base font-medium text-black">
+                Name
+              </label>
+              <input
+                type="text"
+                placeholder="Coffee Name"
+                name="name"
+                className="border-form-stroke text-body-color placeholder-body-color focus:border-primary active:border-primary w-full rounded-lg border-[1.5px] py-3 px-5 font-medium outline-none transition disabled:cursor-default disabled:bg-[#F5F7FD]"
+              />
+            </div>
+            <div className="">
+              <label className="mb-3 ml-1 block text-base font-medium text-black">
+                Chef
+              </label>
+              <input
+                type="text"
+                placeholder="Chef Name"
+                name="chef"
+                className="border-form-stroke text-body-color placeholder-body-color focus:border-primary active:border-primary w-full rounded-lg border-[1.5px] py-3 px-5 font-medium outline-none transition disabled:cursor-default disabled:bg-[#F5F7FD]"
+              />
+            </div>
+            <div className="">
+              <label className="mb-3 ml-1 block text-base font-medium text-black">
+                Supplier
+              </label>
+              <input
+                type="text"
+                placeholder="Coffee Supplier"
+                name="supplier"
+                className="border-form-stroke text-body-color placeholder-body-color focus:border-primary active:border-primary w-full rounded-lg border-[1.5px] py-3 px-5 font-medium outline-none transition disabled:cursor-default disabled:bg-[#F5F7FD]"
+              />
+            </div>
+            <div className="">
+              <label className="mb-3 ml-1 block text-base font-medium text-black">
+                Taste
+              </label>
+              <input
+                type="text"
+                placeholder="Quite Roast Black"
+                name="taste"
+                className="border-form-stroke text-body-color placeholder-body-color focus:border-primary active:border-primary w-full rounded-lg border-[1.5px] py-3 px-5 font-medium outline-none transition disabled:cursor-default disabled:bg-[#F5F7FD]"
+              />
+            </div>
+            <div>
+              <label className="mb-3 block text-base font-medium text-black">
+                Category
+              </label>
+              <div className="relative">
+                <select
+                  name="category"
+                  className="border-form-stroke text-body-color focus:border-primary active:border-primary w-full appearance-none rounded-lg border-[1.5px] py-3 px-5 font-medium outline-none transition disabled:cursor-default disabled:bg-[#F5F7FD]"
+                >
+                  <option value="Rose Coffee">Rose Coffee</option>
+                  <option value="Mac Coffee">Mac Coffee</option>
+                  <option value="Nafa Cafe">Nafa Cafe</option>
+                </select>
+                <span className="border-body-color absolute right-4 top-1/2 mt-[-2px] h-[10px] w-[10px] -translate-y-1/2 rotate-45 border-r-2 border-b-2"></span>
+              </div>
+            </div>
+            <div className="">
+              <label className="mb-3 ml-1 block text-base font-medium text-black">
+                Details
+              </label>
+              <input
+                type="text"
+                placeholder="Details"
+                name="details"
+                className="border-form-stroke text-body-color placeholder-body-color focus:border-primary active:border-primary w-full rounded-lg border-[1.5px] py-3 px-5 font-medium outline-none transition disabled:cursor-default disabled:bg-[#F5F7FD]"
+              />
+            </div>
+            <div className="col-span-2">
+              <label className="mb-3 ml-1 block text-base font-medium text-black">
+                Photo URL
+              </label>
+              <input
+                type="text"
+                placeholder="Photo URL"
+                name="photo"
+                className="border-form-stroke text-body-color placeholder-body-color focus:border-primary active:border-primary w-full rounded-lg border-[1.5px] py-3 px-5 font-medium outline-none transition disabled:cursor-default disabled:bg-[#F5F7FD]"
+              />
+            </div>
+            <input
+              className="btn btn-block col-span-2 my-5"
+              type="submit"
+              value="Submit"
+            />
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import icon1 from "../../public/img/icons/1.png";
+import icon2 from "../../public/img/icons/2.png";
+import icon3 from "../../public/img/icons/3.png";
+import icon4 from "../../public/img/icons/4.png";
 
 const Home = () => {
   return (
@@ -15,6 +19,38 @@ const Home = () => {
             beautiful moments and make them memorable.
           </p>
           <button className="btn">Learn More</button>
+        </div>
+      </div>
+      <div>
+        <div className="py-10 container mx-auto grid grid-cols-1 gap-4 md:grid-cols-4">
+          <div>
+            <img src={icon1} alt="" />
+            <h4 className="text-xl my-3">Awesome Aroma</h4>
+            <p className="w-60">
+              You will definitely be a fan of the design & aroma of your coffee
+            </p>
+          </div>
+          <div>
+            <img src={icon2} alt="" />
+            <h4 className="text-xl my-3">High Quality</h4>
+            <p className="w-60">
+              You will definitely be a fan of the design & aroma of your coffee
+            </p>
+          </div>
+          <div>
+            <img src={icon3} alt="" />
+            <h4 className="text-xl my-3">Pure Grades</h4>
+            <p className="w-60">
+              You will definitely be a fan of the design & aroma of your coffee
+            </p>
+          </div>
+          <div>
+            <img src={icon4} alt="" />
+            <h4 className="text-xl my-3">Proper Roasting</h4>
+            <p className="w-60">
+              You will definitely be a fan of the design & aroma of your coffee
+            </p>
+          </div>
         </div>
       </div>
     </div>
